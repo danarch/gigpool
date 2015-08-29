d29d25c1d541f917fd10a50d49783321d6185cb1
Dylan Massell <dylan.massell@gmail.com>
2015-08-29 16:38:54 -0400

    add company scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150829185459) do
+ActiveRecord::Schema.define(version: 20150829203237) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "first_name"
