be34e21e89541dca716fde4a7336e69a4322263d
Dylan Massell <dylan.massell@gmail.com>
2015-08-29 17:13:05 -0400

    migrate requirements table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150829203237) do
+ActiveRecord::Schema.define(version: 20150829204259) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 20150829203237) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  create_table "requirements", force: :cascade do |t|
+    t.integer  "company_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "requirements", ["company_id"], name: "index_requirements_on_company_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
